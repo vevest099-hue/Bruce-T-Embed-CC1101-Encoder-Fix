aa80259e8442da10b77b41698a087d16ca57dea9
vevest099-hue <vevest099@gmail.com>
2026-07-13 23:11:44 +0300

    Add files via upload

---
+++ src/modules/ble/ble_common.h
@@ -0,0 +1,41 @@
+#ifndef __BLE_COMMON_H__
+#define __BLE_COMMON_H__
+
+// #include "BLE2902.h"
+#include <NimBLEDevice.h>
+#include <NimBLEServer.h>
+#include <NimBLEUtils.h>
+
+#include <NimBLEAdvertisedDevice.h>
+#include <NimBLEBeacon.h>
+#include <NimBLEScan.h>
+
+#include "core/display.h"
+#include <globals.h>
+
+#define SCANTIME 5
+#define SCANTYPE ACTIVE
+#define SCAN_INT 100
+#define SCAN_WINDOW 99
+
+extern BLEScan *pBLEScan;
+extern int scanTime;
+
+void ble_test();
+#if 0 // keep it out for now
+#ifdef BOARD_HAS_PSRAM
+constexpr bool FORCE_RADIO_TEARDOWN_ON_SWITCH = false;
+#else
+constexpr bool FORCE_RADIO_TEARDOWN_ON_SWITCH = true;
+#endif
+#else
+constexpr bool FORCE_RADIO_TEARDOWN_ON_SWITCH = false;
+#endif
+
+void ble_scan_setup();
+void ble_scan();
+void stopBLEStack();
+
+void disPlayBLESend();
+
+#endif
